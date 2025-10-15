5cbc5566a80a9ae50dba110021c21c53dcfb2262
hira-edu <hiraedu01@gmail.com>
2025-10-15 23:15:39 +0500

    ci: grant contents: write; attach portable asset to release; bump version to 1.0.2

--- version_info.py
+++ version_info.py
@@ -2,8 +2,8 @@
 # UTF-8
 VSVersionInfo(
     ffi=FixedFileInfo(
-        filevers=(1, 0, 1, 0),
-        prodvers=(1, 0, 1, 0),
+        filevers=(1, 0, 2, 0),
+        prodvers=(1, 0, 2, 0),
         mask=0x3f,
         flags=0x0,
         OS=0x40004,
@@ -18,12 +18,12 @@ VSVersionInfo(
                 [
                     StringStruct(u'CompanyName', u'Security Research Team'),
                     StringStruct(u'FileDescription', u'Security Testing Framework'),
-                    StringStruct(u'FileVersion', u'1.0.1'),
+                    StringStruct(u'FileVersion', u'1.0.2'),
                     StringStruct(u'InternalName', u'SecurityTestingFramework'),
                     StringStruct(u'LegalCopyright', u'(c) 2024 Security Research Team'),
                     StringStruct(u'OriginalFilename', u'SecurityTestingFramework.exe'),
                     StringStruct(u'ProductName', u'Security Testing Framework'),
-                    StringStruct(u'ProductVersion', u'1.0.1')
+                    StringStruct(u'ProductVersion', u'1.0.2')
                 ]
             )
         ]),
